fdc5dc55f42b0ed8ef2d2be8ae50391414fb9bdb
Clariveljn <cjeldres5@gmail.com>
2025-12-21 18:25:10 -0300

    chore: add Spring Boot backend project

---
+++ backend/churninsight/src/main/java/com/hackathon/churninsight/ChurninsightApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.churninsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurninsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurninsightApplication.class, args);
+	}
+
+}
