9f291db29cc704b636235ac32b10354297195848
Clariveljn <cjeldres5@gmail.com>
2026-01-01 13:15:25 -0300

    feat: add validation to PredictRequestDTO

--- backend/churninsight/src/main/java/com/hackathon/churninsight/dto/request/PredictRequestDTO.java
+++ backend/churninsight/src/main/java/com/hackathon/churninsight/dto/request/PredictRequestDTO.java
@@ -1,10 +1,16 @@
 package com.hackathon.churninsight.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 
 public record PredictRequestDTO(
+        @NotBlank(message = "El customer_id es obligatorio")
         @JsonProperty("customer_id")
         String customerId,
+
+        @NotNull(message = "El objeto features es obligatorio")
+        @Valid
         @JsonProperty("features")
         CustomerFeaturesDTO features
 ) {}
