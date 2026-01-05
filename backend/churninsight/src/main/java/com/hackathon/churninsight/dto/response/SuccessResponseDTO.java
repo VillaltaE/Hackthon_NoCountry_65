658285e32972efd833820c9cb9f3e4f04f550bc2
Clariveljn <cjeldres5@gmail.com>
2026-01-05 14:57:45 -0300

    refactor(dto): remove local configuration and use Jackson's global configuration

--- backend/churninsight/src/main/java/com/hackathon/churninsight/dto/response/SuccessResponseDTO.java
+++ backend/churninsight/src/main/java/com/hackathon/churninsight/dto/response/SuccessResponseDTO.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 
 public record SuccessResponseDTO<T>(
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
         LocalDateTime timestamp,
         int status,
         String message,
